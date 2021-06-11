e2216d86f059b45e4a65fa97bbd86db12b392dc3
jlfarias <jlfarias2009@gmail.com>
2021-06-11 09:49:24 -0300

    Clase pedido con test

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -9,6 +9,34 @@ enum class CodigoHttp(val codigo: Int) {
   NOT_IMPLEMENTED(501),
   NOT_FOUND(404),
 }
+//object CodigoHTTP.OK{}
+//object CodigoHTTP.NOT_IMPLEMENTED{}
+//object CodigoHTTP.NOT_FOUND{}
 
-class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime)
-class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido)
+class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime){
+
+  fun protocolo(): String = url.substringBefore(':')
+  fun ruta(): String = url.substringAfter("com.ar","No Encontrado")
+  fun extension(): String = url.substringAfterLast('.',"No Encontrado")
+
+}
+
+class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido){
+  fun tiempoDeRespuesta(){
+    // tiempo de respuesta en milisegundos
+  }
+  fun codigoDeRespuesta(){
+    // un código de respuesta(Ok, No implementado o no encontrado)
+    //Si el protocolo es distinto a "http" retorna código de respuesta 501 (servicio no implementado)
+  }
+  fun body(){
+    // un body vacio o contenido que será un String
+  }
+  fun referencia(){
+    //una referencia al pedido que la generó.
+  }
+}
+
+
+
+
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -5,6 +5,27 @@ import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
 
 class ServidorWebTest : DescribeSpec({
-  describe("Un servidor web") {
+
+  val fecha = LocalDateTime.of(2021, 6, 10, 14, 17, 22)
+  val order1 = Pedido("207.46.13.5","http://pepito.com.ar/documentos/doc1.html", fecha)
+
+  describe("Un pedido") {
+    describe(" Datos de la URL"){
+      it("El protocolo de la URL es: "){
+        order1.protocolo().shouldBe("http")
+      }
+      it("La ruta de la URL es:"){
+        order1.ruta().shouldBe("/documentos/doc1.html")
+      }
+      it("La extension de la URL del pedido al servidor es"){
+        order1.extension().shouldBe("html")
+      }
+      it("El año del pedido al servidor es"){
+        order1.fechaHora.year.shouldBe(2021)
+      }
+    }
+
+
   }
+
 })
