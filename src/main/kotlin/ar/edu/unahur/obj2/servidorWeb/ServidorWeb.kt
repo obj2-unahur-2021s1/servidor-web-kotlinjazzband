b75d3919bd9bb63384f967dd1856b9c1f3887b45
jlfarias <jlfarias2009@gmail.com>
2021-06-12 16:44:15 -0300

    respuesta y test

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -1,17 +1,8 @@
 package ar.edu.unahur.obj2.servidorWeb
 
+import java.security.cert.Extension
 import java.time.LocalDateTime
 
-// Para no tener los códigos "tirados por ahí", usamos un enum que le da el nombre que corresponde a cada código
-// La idea de las clases enumeradas es usar directamente sus objetos: CodigoHTTP.OK, CodigoHTTP.NOT_IMPLEMENTED, etc
-enum class CodigoHttp(val codigo: Int) {
-  OK(200),
-  NOT_IMPLEMENTED(501),
-  NOT_FOUND(404),
-}
-//object CodigoHTTP.OK{}
-//object CodigoHTTP.NOT_IMPLEMENTED{}
-//object CodigoHTTP.NOT_FOUND{}
 
 class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime){
 
@@ -22,21 +13,8 @@ class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime){
 }
 
 class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido){
-  fun tiempoDeRespuesta(){
-    // tiempo de respuesta en milisegundos
-  }
-  fun codigoDeRespuesta(){
-    // un código de respuesta(Ok, No implementado o no encontrado)
-    //Si el protocolo es distinto a "http" retorna código de respuesta 501 (servicio no implementado)
-  }
-  fun body(){
-    // un body vacio o contenido que será un String
-  }
-  fun referencia(){
-    //una referencia al pedido que la generó.
-  }
+
+  fun verificacionDelProtocolo() =  if (pedido.protocolo() == "http") 200 else 501
+
 }
-
-
-
 
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -6,10 +6,15 @@ import java.time.LocalDateTime
 
 class ServidorWebTest : DescribeSpec({
 
-  val fecha = LocalDateTime.of(2021, 6, 10, 14, 17, 22)
-  val order1 = Pedido("207.46.13.5","http://pepito.com.ar/documentos/doc1.html", fecha)
+  val fecha1 = LocalDateTime.of(2021, 6, 10, 14, 17, 22)
+  val order1 = Pedido("207.46.13.5","http://pepito.com.ar/documentos/doc1.html", fecha1)
+  val order2 = Pedido("207.46.130.7","https://pepito.com.ar/documentos/doc1.html", fecha1)
 
-  describe("Un pedido") {
+  val reply1 = Respuesta(CodigoHttp.OK,"El servicio esta implementado",25,order1)
+  val reply2 = Respuesta(CodigoHttp.NOT_IMPLEMENTED,"Servicio no implementado",10,order2)
+
+  describe("Pedido y Respuesta al servidor sin Módulos") {
+
     describe(" Datos de la URL"){
       it("El protocolo de la URL es: "){
         order1.protocolo().shouldBe("http")
@@ -25,6 +30,15 @@ class ServidorWebTest : DescribeSpec({
       }
     }
 
+    describe("respuesta del servidor al pedido"){
+      it("reply1 respuesta del servidor"){
+        reply1.verificacionDelProtocolo().shouldBe(200)
+      }
+      it("reply2 respuesta del servidor"){
+        reply2.verificacionDelProtocolo().shouldBe(501)
+      }
+
+    }
 
   }
 
