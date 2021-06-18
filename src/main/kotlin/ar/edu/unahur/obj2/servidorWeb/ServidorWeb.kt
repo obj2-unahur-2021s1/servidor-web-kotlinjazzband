d0bfe6ab4765925cc85e027da09c4fb08b35210a
jlfarias <jlfarias2009@gmail.com>
2021-06-18 17:03:10 -0300

    class modulo pasada a Abstract inicio de solucion para mensajes de implementacion

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -7,7 +7,8 @@ object ServidorWeb{
   fun agregarModulo(modulo: Modulo) = modulosHabilitados.add(modulo)
   fun quitarModulo(modulo: Modulo) = modulosHabilitados.remove(modulo)
   fun verificacionDelProtocolo(pedido: Pedido) =  if (pedido.protocolo() == "http") 200 else 501
-  fun moduloAceptaPedido(pedido: Pedido): Boolean = modulosHabilitados.any{  modulo -> modulo.extensioneSoportadas.any{ext -> ext == pedido.extension()} }
+  fun serverAceptaPedido(pedido: Pedido): Boolean = ServidorWeb.modulosHabilitados.any{ modulo -> modulo.extensionesSoportadas.any{ ext -> ext == pedido.extension()} }
+
 }
 
 class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime){
@@ -16,9 +17,67 @@ class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime){
   fun extension(): String = url.substringAfterLast('.',"No Encontrado")
 }
 
-class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido){ }
-
-
-class Modulo(var extensioneSoportadas: MutableList<String>,val texto: String,val tiempo: Int){
-  fun puedeSoportarExtension(extension: String): Boolean = extensioneSoportadas.contains(extension)
+class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido){
+  fun CodigoHttp() = this.codigo
+  fun body() = this.body
+  fun tiempo() = this.tiempo
+  fun pedido() = this.pedido
 }
+
+abstract class Modulo(pedido: Pedido) {
+
+  var mensaje: Mensajes = RtaError
+
+  val extensionesSoportadas: MutableList<String> = mutableListOf()
+  fun agregarExtension(extension: String) = extensionesSoportadas.add(extension)
+  fun puedeSoportarExtension(extension: String): Boolean = extensionesSoportadas.contains(extension)
+
+  fun rtaParaPedido(pedido:Pedido){
+    if(ServidorWeb.serverAceptaPedido(pedido)) mensaje = RtaOK
+  }
+
+}
+
+class Image(pedido: Pedido):Modulo(pedido){}
+class Texto(pedido: Pedido):Modulo(pedido){}
+class Video(pedido: Pedido):Modulo(pedido){}
+
+
+interface Mensajes {
+  fun codigo():CodigoHttp
+  fun body(): String
+  fun tiempo(): Int
+}
+object RtaOK: Mensajes{
+  override fun codigo() = CodigoHttp.OK
+  override fun body(): String = "Servicio Implementado"
+  override fun tiempo(): Int = 45
+}
+object RtaNoEncontrado: Mensajes{
+  override fun codigo() = CodigoHttp.NOT_IMPLEMENTED
+  override fun body(): String = " "
+  override fun tiempo(): Int = 10
+}
+object RtaError: Mensajes{
+  override fun codigo() = CodigoHttp.NOT_FOUND
+  override fun body(): String = " "
+  override fun tiempo(): Int = 10
+}
+
+
+
+
+/*
+
+  if(moduloAceptaPedido(pedido)){
+    refPedido = pedido
+    codigo = CodigoHttp.OK
+    body = "Servicio Implementado"
+    tiempo = 15
+  }
+  var codigo: CodigoHttp = CodigoHttp.NOT_IMPLEMENTED
+  var body: String = "Servicio No Implementado"
+  var tiempo: Int = 10
+  var refPedido: Pedido = pedido
+
+ */
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -4,6 +4,7 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import java.time.LocalDateTime
 
 class ServidorWebTest : DescribeSpec({
@@ -43,13 +44,30 @@ class ServidorWebTest : DescribeSpec({
   val order3 = Pedido("207.46.13.8","http://pepito.com.ar/documentos/doc1.jpg", fecha2)
   val order4 = Pedido("207.46.130.9","http://pepito.com.ar/documentos/doc1.html", fecha2) // era https
   val order5 = Pedido("207.46.13.5","http://pepito.com.ar/documentos/doc1.avi", fecha1)
+  val order6 = Pedido("207.46.130.9","http://pepito.com.ar/documentos/doc1.css", fecha2)
+  val order7 = Pedido("207.46.13.5","http://pepito.com.ar/documentos/doc1.json", fecha1)
 
   //val reply3 = Respuesta(CodigoHttp.OK,"El servicio esta implementado",25,order3)
   //val reply4 = Respuesta(CodigoHttp.OK,"El servicio esta implementado",25,order4)
+  //val unModuloImagen= Modulo(mutableListOf("jpg","png","gif"),"Es de imagen",10)
+  //val unModuloTexto= Modulo(mutableListOf("docx","odt","txt","html"),"Es de texto",10)
+  //val unModuloVideo= Modulo(mutableListOf("mpg","avi","mpeg"),"Es de video",10)
 
-  val unModuloImagen= Modulo(mutableListOf("jpg","png","gif"),"Es de imagen",10)
-  val unModuloTexto= Modulo(mutableListOf("docx","odt","txt","html"),"Es de texto",10)
-  val unModuloVideo= Modulo(mutableListOf("mpg","avi","mpeg"),"Es de video",10)
+
+  val unModuloImagen = Image(order3)
+  unModuloImagen.agregarExtension("jpg")
+  unModuloImagen.agregarExtension("png")
+  unModuloImagen.agregarExtension("gif")
+
+  val unModuloTexto = Texto(order4)
+  unModuloTexto.agregarExtension("docx")
+  unModuloTexto.agregarExtension("odt")
+  unModuloTexto.agregarExtension("txt")
+  unModuloTexto.agregarExtension("html")
+  val unModuloVideo = Video(order5)
+  unModuloVideo.agregarExtension("mpg")
+  unModuloVideo.agregarExtension("avi")
+  unModuloVideo.agregarExtension("mpeg")
 
   //Agregamos a modulo habilitados en ServidorWeb
   ServidorWeb.modulosHabilitados.add(unModuloImagen)
@@ -74,12 +92,24 @@ class ServidorWebTest : DescribeSpec({
       unModuloVideo.puedeSoportarExtension("avi").shouldBeTrue()
       unModuloVideo.puedeSoportarExtension("mpeg").shouldBeTrue()
     }
-    //order3 = jpg   order4 = html     order5 = avi
-    it("Verificar si ServidorWeb acepta imagenes y texto y rechaza video por falt ade modulo"){
-      ServidorWeb.moduloAceptaPedido(order3).shouldBeTrue()   //es jpg imagen
-      ServidorWeb.moduloAceptaPedido(order4).shouldBeTrue()  // es html texto
-      ServidorWeb.moduloAceptaPedido(order5).shouldBeFalse()   // es avi video
+
+    it("Verificar si ServidorWeb acepta imagenes y texto y rechaza video por falta de modulo"){
+      ServidorWeb.serverAceptaPedido(order3).shouldBeTrue()     //es raw imagen
+      ServidorWeb.serverAceptaPedido(order4).shouldBeTrue()     // es html texto
+      ServidorWeb.serverAceptaPedido(order5).shouldBeFalse()   // es avi extension correcta modulo video no agregado
+      ServidorWeb.serverAceptaPedido(order6).shouldBeFalse()   // es css video
+      ServidorWeb.serverAceptaPedido(order7).shouldBeFalse()   // es json video
     }
+
+    it("Probar el mensaje de acepta"){
+      (unModuloVideo.mensaje.body() == " ").shouldBeTrue()
+      (unModuloVideo.mensaje.codigo() == CodigoHttp.NOT_FOUND).shouldBeTrue()
+      (unModuloVideo.mensaje.tiempo() == 10 ).shouldBeTrue()
+    }
+  }
+
+  describe("Probar Mensaje"){
+
   }
 
 })
